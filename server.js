dd1aa443704a98e7bcb0ff4e853b1fe1dd2e2d11
Christian Garlen <chadgarval@outlook.com>
2026-09-24 19:14:52 -0400

    feat: initialize database connection with connectDB before starting server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 // Dependencies
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connection.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const uri = process.env.MONGO_URI;
 
+
+
+
+// Listener
+// Call the connection to the database first, then call the listener
+// to avoid listening the server before the database is connected
+
+await connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
